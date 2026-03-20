41f445f3cd46c51a778865b6dbefdc11533bcc36
basiacarvalho <basiacarvalho@gmail.com>
2026-03-20 15:26:04 +0100

    adding setTimeout for requesting loan

--- script.js
+++ script.js
@@ -326,13 +326,13 @@ btnLoan.addEventListener('click', function (e) {
   const amount = Math.floor(inputLoanAmount.value);
 
   if (amount > 0 && currentAccount.movements.some(mov => mov >= amount * 0.1)) {
-    currentAccount.movements.push(amount);
-
-    //Add loan date
-    currentAccount.movementsDates.push(new Date().toISOString());
-
-    updateUI(currentAccount);
+    setTimeout(function () {
+      currentAccount.movements.push(amount);
+      currentAccount.movementsDates.push(new Date().toISOString());
+      updateUI(currentAccount);
+    }, 2500);
   }
+
   inputLoanAmount.value = '';
 });
 
